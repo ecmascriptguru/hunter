7e54d8a78ec874b9fd7eeffcbceaa4f435de7995
Alexis Richard <ecmascript.guru@gmail.com>
2019-01-05 11:08:59 +0200

    static conf update

--- config/settings/base.py
+++ config/settings/base.py
@@ -132,9 +132,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = ( join('static'), )
+STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'), )
+
+
 # Custom authentication configuration
 AUTH_USER_MODEL = 'users.User'
 AUTHENTICATION_BACKENDS = ('email_hunter.core.backends.CaseInsensitiveModelBackend', )
