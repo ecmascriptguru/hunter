0c726d25fee3e38b8fda73fe2802080c8df0db83
Perfect Delivery <ecmascript.guru@gmail.com>
2019-02-22 22:53:06 +0200

    timelimit exceed issue handling module added. (testing required)

--- email_hunter/apps/articles/tasks.py
+++ email_hunter/apps/articles/tasks.py
@@ -1,5 +1,6 @@
 import time
 from celery import shared_task
+from celery.exceptions import TimeLimitExceeded, TimeoutError
 from ...core import Article as Hunter, ArticleException as HunterException
 from ...core.spiders.browser import Chrome
 from .models import Article, ARTICLE_STATE, Bucket, BUCKET_STATE
@@ -69,64 +70,76 @@ def extract_authors(self, bucket_id, article_ids):
     self.update_state(state="PROGRESS", meta=DEFAULT_META)
     if not isinstance(article_ids, list):
         return DEFAULT_META
-    
-    browser = Chrome()
-
-    for id in article_ids:
-        article = Article.objects.get(pk=id)
-        article.state = ARTICLE_STATE.in_progress
-        article.save()
-
-        # TODO: Should extract author
-        hunter = Hunter(article.url)
-        browser.get(article.url)
-        time.sleep(1)
         
-        # hunter.download()
-        hunter.download(input_html=browser.page_source)
+    try:
+        browser = Chrome()
 
-        try:
-            hunter.parse()
-        except HunterException as e:
-            print(str(e))
-            if article.url.startswith(HTTPS_PROTOCOL_PREFIX):
-                try_with_http_protocol(article, browser)
-            else:
-                article.state = ARTICLE_STATE.page_not_found
-        except Exception as e:
-            print(str(e))
-            if article.url.startswith(HTTPS_PROTOCOL_PREFIX):
-                try_with_http_protocol(article)
-            else:
-                article.state = ARTICLE_STATE.has_error
-        else:
-            authors = hunter.authors
-            if len(authors) > 0:
-                article.authors['candidates'] = authors
-                if len(authors) > 1:
-                    article.state = ARTICLE_STATE.need_confirm
-                else:
-                    article.authors['found'] = authors[0]
-                    article.state = ARTICLE_STATE.found
-            else:
-                if bucket.is_test_data:
-                    if article.authors.get('origin').lower() in hunter.text.lower() or\
-                            article.authors.get('origin').lower() in hunter.html.lower():
-                        article.state = ARTICLE_STATE.not_found
-                    else:
-                        article.state = ARTICLE_STATE.no_author
-                else:
-                    article.state = ARTICLE_STATE.not_found
-        finally:
+        for id in article_ids:
+            article = Article.objects.get(pk=id)
+            article.state = ARTICLE_STATE.in_progress
             article.save()
-            DEFAULT_META.update({'current': DEFAULT_META.get('current') + 1})
-            self.update_state(state="PROGRESS", meta=DEFAULT_META)
-    
-    self.update_state(state="COMPLETE", meta=DEFAULT_META)
 
-    if not bucket.articles.filter(state__in=[ARTICLE_STATE.in_progress, ARTICLE_STATE.pending]).exists():
-        bucket.state = BUCKET_STATE.default
-        bucket.save()
+            # TODO: Should extract author
+            hunter = Hunter(article.url)
+            browser.get(article.url)
+            time.sleep(1)
+            
+            # hunter.download()
+            hunter.download(input_html=browser.page_source)
 
-    browser.quit()
-    return DEFAULT_META
+            try:
+                hunter.parse()
+            except HunterException as e:
+                print(str(e))
+                if article.url.startswith(HTTPS_PROTOCOL_PREFIX):
+                    try_with_http_protocol(article, browser)
+                else:
+                    article.state = ARTICLE_STATE.page_not_found
+            except Exception as e:
+                print(str(e))
+                if article.url.startswith(HTTPS_PROTOCOL_PREFIX):
+                    try_with_http_protocol(article)
+                else:
+                    article.state = ARTICLE_STATE.has_error
+            else:
+                authors = hunter.authors
+                if len(authors) > 0:
+                    article.authors['candidates'] = authors
+                    if len(authors) > 1:
+                        article.state = ARTICLE_STATE.need_confirm
+                    else:
+                        article.authors['found'] = authors[0]
+                        article.state = ARTICLE_STATE.found
+                else:
+                    if bucket.is_test_data:
+                        if article.authors.get('origin').lower() in hunter.text.lower() or\
+                                article.authors.get('origin').lower() in hunter.html.lower():
+                            article.state = ARTICLE_STATE.not_found
+                        else:
+                            article.state = ARTICLE_STATE.no_author
+                    else:
+                        article.state = ARTICLE_STATE.not_found
+            finally:
+                article.save()
+                DEFAULT_META.update({'current': DEFAULT_META.get('current') + 1})
+                self.update_state(state="PROGRESS", meta=DEFAULT_META)
+        
+    except TimeLimitExceeded as e:
+        print(str(e))
+        remaining = list()
+        for id in article_ids:
+            article = Article.objects.get(pk=id)
+            if article.state in [ARTICLE_STATE.pending, ARTICLE_STATE.in_progress]:
+                article.state = ARTICLE_STATE.pending
+                article.save()
+                remaining.append(article.pk)
+        extract_authors.delay(bucket.pk, remaining)
+    finally:
+        self.update_state(state="COMPLETE", meta=DEFAULT_META)
+
+        if not bucket.articles.filter(state__in=[ARTICLE_STATE.in_progress, ARTICLE_STATE.pending]).exists():
+            bucket.state = BUCKET_STATE.default
+            bucket.save()
+
+        browser.quit()
+        return DEFAULT_META
